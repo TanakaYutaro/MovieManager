6c59020b82ff5cb07826800be4a6f40b5e7e7a19
yutaro <yutaro.tanaka.world@gmail.com>
2014-12-10 00:36:45 +0900

    Emailフォーマットの検証を追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -44,4 +44,24 @@ describe "User" do
       expect(@user).not_to be_valid
     end
   end
+
+  describe "when email format is invalid" do
+    it "shoud be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user).not_to be_valid
+      end
+    end
+  end
+
+  describe "when email format is valid" do
+    it "shoud be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid
+      end
+    end
+  end
 end
