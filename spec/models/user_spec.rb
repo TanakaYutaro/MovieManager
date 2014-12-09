c2c3f7a9b38dc1bea2a816096ab30cb99127d50a
yutaro <yutaro.tanaka.world@gmail.com>
2014-12-10 00:20:21 +0900

    モデルプロパティの存在検証追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 require 'rails_helper'
 
 describe "User" do
-  before {
+  before do
     @user = User.new(name: "Example User", email: "user@example.com")
-  }
+  end
 
-  subject {
+  subject do
     @user
-  }
+  end
 
   it "should respond to 'name'" do
     expect(@user).to respond_to(:name)
@@ -18,4 +18,21 @@ describe "User" do
     expect(@user).to respond_to(:email)
   end
 
+  describe "when name is not present" do
+    before do
+      @user.name = ""
+    end
+    it "shoud not be valid" do
+      expect(@user).not_to be_valid
+    end
+  end
+
+  describe "when email is not present" do
+    before do
+      @user.email = ""
+    end
+    it "shoud not be valid" do
+      expect(@user).not_to be_valid
+    end
+  end
 end
